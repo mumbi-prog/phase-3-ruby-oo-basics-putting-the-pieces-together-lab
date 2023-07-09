32a560756a3adb1049e8c70efdf88aef2d9fd1fc
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-09 10:46:31 +0300

    Done with lab test

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
-
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+    def initialize(title="And Then There Were None", author="Agatha Christie", page_count=272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+mybook = Book.new
+#binding.pry
